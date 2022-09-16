ecadc00cfd8ae5a3eb3fa1ea448c9dd7ba0bea2d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-16 22:11:23 +0300

    feat: add 2 lesson

---
+++ Jest and Enzyme/02 Snapshot testing/src/Posts/posts.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Posts from "./posts";
+
+describe("Posts component", () => {
+  it("should render Post component", () => {
+    // const component = shallow(<Posts />);
+    const component = render(<Posts />);
+    expect(component).toMatchSnapshot();
+  });
+});
