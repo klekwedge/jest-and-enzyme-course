e322f1b19b7e4fad7d989239890e5ea3a9c87eb9
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-25 21:42:10 +0300

    feat: add 4 lesson

---
+++ Jest and Enzyme/04 Testing Change Events/src/Posts/posts.spec.js
@@ -0,0 +1,46 @@
+import { shallow } from "enzyme";
+import React from "react";
+import Posts from "./posts";
+
+const setUp = () => shallow(<Posts />);
+
+describe("Posts component", () => {
+  const DEFAULT_PAGE = 10;
+  let component;
+  let instance;
+
+  beforeEach(() => {
+    component = setUp();
+    instance = component.instance();
+  });
+
+  it("should render Post component", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  describe("Posts handlers", () => {
+    it("should handle search input value", () => {
+      expect(component.state().searchQuery).toBe("");
+      instance.handleInputChange({ target: { value: "test" } });
+      expect(component.state().searchQuery).toBe("test");
+    });
+
+    it("should handle change hits per page", () => {
+      expect(component.state().hitsPerPage).toBe(20);
+      instance.handleHitsChange({ target: { value: String(DEFAULT_PAGE) } });
+      expect(component.state().hitsPerPage).toBe(DEFAULT_PAGE);
+    });
+
+    it("should handle change page if 'Enter' clicked", () => {
+      instance.setState({ page: DEFAULT_PAGE });
+      instance.getSearch({ key: "Enter" });
+      expect(component.state().page).toBe(0);
+    });
+
+    it("should handle change page if 'a' button is clicked", () => {
+      instance.setState({ page: DEFAULT_PAGE });
+      instance.getSearch({ key: "a" });
+      expect(component.state().page).toBe(DEFAULT_PAGE);
+    });
+  });
+});
